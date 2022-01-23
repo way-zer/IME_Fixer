138010f156c2a648a298ac2c1485231a31f1c092
way-zer <himc.wicp@gmail.com>
2022-01-23 19:40:39 +0800

    修复链接问题

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # IME Fixer
 A mod in order to resolve IME problem in windows
 一个尝试解决Windows下中文输入法问题的MOD
+# Features
+- [x] fix show of candidate list 修复候选框不显示
+- [x] fix show of composition list 修复拼写框不显示
+- [x] fix position of the candidate list 修复输入法显示位置(聊天/指令)
+- [ ] auto enable/disable input method 游戏时自动关闭输入法
+- [ ] fix `backspace` when composite 修复拼写时删除问题
 ## Quick Start
-Still work in place (暂不可用,仍在开发实验中)
+Install the jar into `mods` (Only support win64)
+安装jar到mods(目前仅支持WIN64系统)
 ## 开发
-开发环境 IDEA JAVA
-相关技术 JNA Windows(imm API)
+开发环境 IDEA JAVA MINGW
+相关技术 JNI WindowsAPI(imm32 msctf)
 环境配置 导入仓库到IDEA中,使用IDEA进行编译,打包
--- jni/compile.sh
+++ jni/compile.sh
@@ -1,9 +1,15 @@
+export JAVA_HOME=/mnt/c/Program\ Files/Java/jdk1.8.0_212/
+
 mkdir out
-i686-w64-mingw32-g++ -shared -Wall -Wl,--kill-at \
-  -I$JAVA_HOME/include -I$JAVA_HOME/include/win32 \
-  -o out/ime_Fixer.dll *.cpp \
-  -limm32 -lmsctf
-x86_64-w64-mingw32-g++ -shared -Wall -Wl,--kill-at \
+echo Compile x86
+i686-w64-mingw32-g++ -shared -Wall -Wl,--kill-at -time -s \
   -I$JAVA_HOME/include -I$JAVA_HOME/include/win32 \
   -o out/ime_Fixer64.dll *.cpp \
-  -limm32 -L ./lib -lmsctf
+  -Wl,-Bdynamic -limm32 -lmsctf \
+  -Wl,-Bstatic -lstdc++ -lpthread -static-libgcc -static-libstdc++
+echo Compile x64
+x86_64-w64-mingw32-g++ -shared -Wall -Wl,--kill-at -time -s \
+  -I$JAVA_HOME/include -I$JAVA_HOME/include/win32 \
+  -o out/ime_Fixer64.dll *.cpp -L ./lib \
+  -Wl,-Bdynamic -limm32 -lmsctf \
+  -Wl,-Bstatic -lstdc++ -lpthread -static-libgcc -static-libstdc++
